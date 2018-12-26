3856b5c003cd7295369866709546eb75091f938b
BlueArduino20 <bluearduino20@gmail.com>
2018-12-26 15:27:00 +0100

    Update get-pass.sh

--- get-pass.sh
+++ get-pass.sh
@@ -1,23 +1,10 @@
 # TERMINAL COLORS
 # https://github.com/BlueArduino20/terminal-colors
-NO_COLOR="\e[0m"
-WHITE="\e[0;17m"
 BOLD_WHITE="\e[1;37m"
-BLACK="\e[0;30m"
-BLUE="\e[0;34m"
-BOLD_BLUE="\e[1;34m"
-GREEN="\e[0;32m"
 BOLD_GREEN="\e[1;32m"
-CYAN="\e[0;36m"
 BOLD_CYAN="\e[1;36m"
 RED="\e[0;31m"
 BOLD_RED="\e[1;31m"
-PURPLE="\e[0;35m"
-BOLD_PURPLE="\e[1;35m"
-BROWN="\e[0;33m"
-BOLD_YELLOW="\e[1;33m"
-GRAY="\e[0;37m"
-BOLD_GRAY="\e[1;30m"
 # END OF TERMINAL COLORS
 
 echo -e -n "$BOLD_GREEN Write the target IP > $BOLD_WHITE "
